3c01ccb1a39678aa823bfbcbe21c6a157ded05bc
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2020-04-09 14:47:33 +0200

    Fix the poll operations.

--- src/main/kotlin/io/github/airvision/util/collections/MutableCollection.kt
+++ src/main/kotlin/io/github/airvision/util/collections/MutableCollection.kt
@@ -10,46 +10,89 @@
 package io.github.airvision.util.collections
 
 fun <E> MutableCollection<E>.poll(n: Int): List<E> {
-  return pollWhile(n) { true }
-}
-
-fun <E> MutableCollection<E>.pollWhile(n: Int, fn: (E) -> Boolean): List<E> {
   require(n >= 0) { "Requested element count $n is less than zero." }
-  if (n == 0)
+  if (isEmpty())
     return emptyList()
-  return iterator().pollWhile(n, fn)
-}
-
-fun <E> MutableList<E>.pollLast(n: Int): List<E> {
-  return pollLastWhile(n) { true }
-}
-
-fun <E> MutableList<E>.pollLastWhile(n: Int, fn: (E) -> Boolean): List<E> {
-  require(n >= 0) { "Requested element count $n is less than zero." }
-  if (n == 0)
-    return emptyList()
-  return reverseIterator().pollWhile(n, fn)
+  return iterator().poll(n)
 }
 
 fun <E> MutableIterator<E>.poll(n: Int): List<E> {
-  return pollWhile(n) { true }
-}
-
-fun <E> MutableIterator<E>.pollWhile(n: Int, fn: (E) -> Boolean): List<E> {
   require(n >= 0) { "Requested element count $n is less than zero." }
   if (n == 0 || !hasNext())
     return emptyList()
+  if (n == 1)
+    return listOf(next())
   var remaining = n
   val result = mutableListOf<E>()
   while (hasNext() && remaining > 0) {
+    remaining--
+    result.add(next())
+    remove()
+  }
+  return result
+}
+
+fun <E> MutableCollection<E>.pollWhile(fn: (E) -> Boolean): List<E> {
+  if (isEmpty())
+    return emptyList()
+  return iterator().pollWhile(fn)
+}
+
+fun <E> MutableIterator<E>.pollWhile(fn: (E) -> Boolean): List<E> {
+  if (!hasNext())
+    return emptyList()
+  val result = mutableListOf<E>()
+  while (hasNext()) {
     val value = next()
     if (!fn(value))
-      continue
-    remaining--
+      break
     result.add(value)
     remove()
   }
   return result
+}
+
+fun <E> MutableList<E>.pollLast(n: Int): List<E> {
+  require(n >= 0) { "Requested element count $n is less than zero." }
+  if (isEmpty() || n == 0)
+    return emptyList()
+  if (n == 1)
+    return listOf(removeLast())
+  if (n >= size) {
+    val list = toList()
+    clear()
+    return list
+  }
+  val list = ArrayList<E>(n)
+  val index = size - n
+  for (i in 0 until n)
+    list.add(removeAt(index))
+  return list
+}
+
+fun <E> MutableList<E>.pollLastWhile(fn: (E) -> Boolean): List<E> {
+  if (isEmpty())
+    return emptyList()
+  val it = listIterator(size)
+  while (it.hasPrevious()) {
+    if (!fn(it.previous())) {
+      it.next()
+      val expectedSize = size - it.nextIndex()
+      if (expectedSize == 0)
+        return emptyList()
+      if (expectedSize == 0)
+        return listOf(removeLast())
+      val list = ArrayList<E>(expectedSize)
+      while (it.hasNext()) {
+        add(it.next())
+        it.remove()
+      }
+      return list
+    }
+  }
+  val list = toList()
+  clear()
+  return list
 }
 
 @JvmName("reverseMutableIterator")
